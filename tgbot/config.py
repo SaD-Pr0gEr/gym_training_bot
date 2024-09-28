95c01aad57404464757eaea017d0d2c369c50da8
SaD-Pr0gEr <sad.pr0ger@gmail.com>
2024-09-28 15:29:57 +0000

    db config divide

--- tgbot/config.py
+++ tgbot/config.py
@@ -6,9 +6,52 @@ from environs import Env
 @dataclass
 class DbConfig:
     host: str
+    port: int
+
+    @property
+    def db_url(self) -> str:
+        raise NotImplementedError
+
+    @property
+    def db_url_async(self) -> str:
+        raise NotImplementedError
+
+
+@dataclass
+class PgDbConfig(DbConfig):
     password: str
     user: str
     database: str
+
+    @property
+    def db_url(self) -> str:
+        return (
+            f'postgresql://{self.user}:{self.password}@'
+            f'{self.host}:{self.port}/{self.database}'
+        )
+
+    @property
+    def db_url_async(self) -> str:
+        return f'postgresql+asyncpg://' + self.db_url.split('//')[-1]
+
+
+@dataclass
+class RedisDbConfig(DbConfig):
+    password: str | None = None
+    user: str | None = None
+    database: str = '1'
+
+    @property
+    def db_url(self) -> str:
+        url = 'redis://'
+        if self.user is not None and self.password is not None:
+            url += f'{self.user}:{self.password}@'
+        url += f'f{self.host}:{self.port}/{self.database}'
+        return url
+
+    @property
+    def db_url_async(self) -> str:
+        return self.db_url
 
 
 @dataclass
@@ -35,8 +78,9 @@ def load_config(path: str | None = None):
             admin_ids=env.list('ADMINS'),
             use_redis=env.bool('USE_REDIS'),
         ),
-        db=DbConfig(
+        db=PgDbConfig(
             host=env.str('DB_HOST'),
+            port=env.str('DB_PORT'),
             password=env.str('DB_PASS'),
             user=env.str('DB_USER'),
             database=env.str('DB_NAME')
